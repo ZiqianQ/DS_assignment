5cf87692451474f4b19a5198e4363d8f93e36d33
Mike <tinshou@hotmail.com>
2017-04-29 20:10:56 +1000

    Update exchangeCommand.java

--- DSProject4.0/src/Client/exchangeCommand.java
+++ DSProject4.0/src/Client/exchangeCommand.java
@@ -9,6 +9,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 
+import org.apache.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -16,7 +17,7 @@ import org.json.simple.parser.ParseException;
 
 public class exchangeCommand {
 	
-	public static void execute (String ip, int port){
+	public static void execute (String ip, int port, String getservers){
 		try(Socket socket = new Socket(ip,port)){
 			//output stream
 			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
@@ -26,36 +27,43 @@ public class exchangeCommand {
 			JSONObject newCommand = new JSONObject();
 
 			newCommand.put("command", "EXCHANGE");
-			JSONObject host1 = new JSONObject();
-			JSONObject host2 = new JSONObject();
-			//host name one
-			host1.put("hostname", "192.168.1.1");
-			host1.put("port", 3000);
-			//host name two
-			host2.put("hostname", "192.168.0.1");
-			host2.put("port", 3000);
-
-			JSONArray list = new JSONArray();
-			list.add(host1);
-			list.add(host2);
-
-			newCommand.put("serverList", list);
 			
-			//print JSONObject
-			Client.logger.info("SENT:");
-			System.out.println(newCommand.toJSONString());
-			
-			//send command to server
-			output.writeUTF(newCommand.toJSONString());
-			output.flush();
-			//read from server and print out
-			while(true){
-				if (input.available()>0) {
-					String result = input.readUTF();
-					Client.logger.info("REVEIVED:");
-					System.out.println(result);
+			if (getservers!=null) {
+				String[] serverList = getservers.split(",");
+				for (int i = 0; i < serverList.length; i++) {
+					String host = serverList[i].split(":")[0];
+					String ports = serverList[i].split(":")[1];
+					JSONObject server = new JSONObject();
+					server.put("hostname", host);
+					server.put("port", ports);
+					JSONArray serve = new JSONArray();
+					serve.add(server);
+					newCommand.put("serverList", serve);
 				}
+				
+				
+				//print JSONObject
+				Client.logger.info("SENT:");
+				System.out.println(newCommand.toJSONString());
+				
+				//send command to server
+				output.writeUTF(newCommand.toJSONString());
+				output.flush();
+				//read from server and print out
+				while(true){
+					if (input.available()>0) {
+						String result = input.readUTF();
+						Client.logger.info("REVEIVED:");
+						System.out.println(result);
+					}
+				}
+				
 			}
+			else {
+				Client.logger.info("ERROR: missing serverlists");
+				
+			}
+
 			
 		}catch (UnknownHostException e) {
 			e.printStackTrace();
