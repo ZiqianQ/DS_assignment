204f999c25fe03cd68c49d8812e9cdd576b4b978
Mike <tinshou@hotmail.com>
2017-05-28 04:15:02 +1000

    Update sendMessage.java

--- DsProject2.2.0/src/Client/sendMessage.java
+++ DsProject2.2.0/src/Client/sendMessage.java
@@ -7,20 +7,39 @@ import java.io.RandomAccessFile;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class sendMessage {
-	
+
+	public static JSONObject unsubscribeCommand = new JSONObject();
+
 	public static void sender(String ip, int port,JSONObject newCommand, boolean debugMode) throws ParseException{
 		try(Socket socket = new Socket(ip,port)){
 			//output stream
 			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 			//input stream
-			DataInputStream input = new DataInputStream(socket.getInputStream());
-			
+			DataInputStream input = new DataInputStream(socket.getInputStream());			
+
+
+			 Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+			 logger.setLevel(Level.OFF);
+		        try { 
+		            GlobalScreen.registerNativeHook();
+		            
+		        } catch (NativeHookException ex) {
+		            ex.printStackTrace();	              
+		        }
+		       GlobalScreen.addNativeKeyListener(new keylogger());
+		     
 			
 			
 			//send command to server
@@ -28,11 +47,31 @@ public class sendMessage {
 			output.flush();
 
 			boolean send = false;
-						
+			/////exit the subscribe	
+			boolean exit = false;
 				JSONParser parser = new JSONParser();
-				
+				JSONObject getcommand = new JSONObject();
 				//read from server and print out
 				while(true){
+//					System.out.println("before isempty:"+unsubscribeCommand.isEmpty());
+					if (!unsubscribeCommand.isEmpty()) {
+//						System.out.println("after isempty:"+unsubscribeCommand.isEmpty());
+						//send unsubscribe command to server
+						output.writeUTF(unsubscribeCommand.toJSONString());
+						output.flush();
+						while (input.available()>0) {
+							getcommand = (JSONObject) parser.parse(input.readUTF());
+							Client.logger.info(getcommand.toJSONString());
+							socket.close();
+							exit = true;
+							break;
+						}
+					}
+					
+					if (exit) {
+						break;
+					}
+					
 					if (input.available()>0) {
 						String result = input.readUTF();
 						Client.logger.info("RECEIVED:"+result);
@@ -86,6 +125,7 @@ public class sendMessage {
 			e.printStackTrace();
 		}
 	}
+
 	
 	//ONLY USEFUL WHEN ITS FETCH COMMAND
 	private static int setChunkSize(long fileSizeRemaining) {
