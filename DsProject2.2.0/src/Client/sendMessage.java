94b4a3b401a79a0a268a77f937e3df64e2c8efe3
Mike <tinshou@hotmail.com>
2017-05-27 19:28:49 +1000

    Update sendMessage.java

--- DsProject2.2.0/src/Client/sendMessage.java
+++ DsProject2.2.0/src/Client/sendMessage.java
@@ -13,91 +13,90 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class sendMessage {
-
-	public static void sender(String ip, int port, JSONObject newCommand, boolean debugMode) throws ParseException {
-		try (Socket socket = new Socket(ip, port)) {
-			// output stream
+	
+	public static void sender(String ip, int port,JSONObject newCommand, boolean debugMode) throws ParseException{
+		try(Socket socket = new Socket(ip,port)){
+			//output stream
 			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-			// input stream
+			//input stream
 			DataInputStream input = new DataInputStream(socket.getInputStream());
-
-			// send command to server
+			
+			
+			
+			//send command to server
 			output.writeUTF(newCommand.toJSONString());
 			output.flush();
 
-			JSONParser parser = new JSONParser();
-
-			// read from server and print out
-			while (true) {
-				if (input.available() > 0) {
-					String result = input.readUTF();
-					Client.logger.info("RECEIVED:");
-					System.out.println(result);
-			
-			
+			boolean send = false;
+						
+				JSONParser parser = new JSONParser();
 				
-				    //String result = input.readUTF();
-					// ONLY USEFUL WHEN ITS FETCH COMMAND
-					JSONObject command = (JSONObject) parser.parse(result);
-					if (command.containsKey("resourceSize")) {
-						String fileName = "client_files/" + (String) command.get("name");
-
-						// The file location
-						// Create a RandomAccessFile to read and write the
-						// output file.
-						RandomAccessFile downloadingFile = new RandomAccessFile(fileName, "rw");
-						// Find out how much size is remaining to get from the
-						// server.
-						long fileSizeRemaining = (Long) command.get("resourceSize");
-						int chunkSize = setChunkSize(fileSizeRemaining);
-
-						// Represents the receiving buffer
-						byte[] receiveBuffer = new byte[chunkSize];
-						// Variable used to read if there are remaining size
-						// left to read.
-						int num;
-						while ((num = input.read(receiveBuffer)) > 0) {
-							// Write the received bytes into the
-							// RandomAccessFile
-							downloadingFile.write(Arrays.copyOf(receiveBuffer, num));
-
-							// Reduce the file size left to read..
-							fileSizeRemaining -= num;
-
-							// Set the chunkSize again
-							chunkSize = setChunkSize(fileSizeRemaining);
-							receiveBuffer = new byte[chunkSize];
-
-							// If you're done then break
-							if (fileSizeRemaining == 0) {
-								break;		
-							}
-						}
-						downloadingFile.close();
-					}
+				//read from server and print out
+				while(true){
+					if (input.available()>0) {
+						String result = input.readUTF();
+						Client.logger.info("RECEIVED:"+result);
+						send=true;
+						
+						//ONLY USEFUL WHEN ITS FETCH COMMAND
+						JSONObject command = (JSONObject) parser.parse(result);	
+						if(command.containsKey("resourceSize")){ 
+			 				String fileName ="client_files/"+ (String) command.get("name"); 
+			 				 
+		 					// The file location
+								// Create a RandomAccessFile to read and write the output file.
+								RandomAccessFile downloadingFile = new RandomAccessFile(fileName, "rw");
+								// Find out how much size is remaining to get from the server.
+								long fileSizeRemaining = (Long) command.get("resourceSize");
+								int chunkSize = setChunkSize(fileSizeRemaining);
+								
+								// Represents the receiving buffer
+								byte[] receiveBuffer = new byte[chunkSize];
+								// Variable used to read if there are remaining size left to read.
+								int num; 
+								while((num=input.read(receiveBuffer))>0){
+									// Write the received bytes into the RandomAccessFile
+									downloadingFile.write(Arrays.copyOf(receiveBuffer, num));
+									
+									// Reduce the file size left to read..
+									fileSizeRemaining-=num;
+									
+									// Set the chunkSize again
+									chunkSize = setChunkSize(fileSizeRemaining);
+									receiveBuffer = new byte[chunkSize];
+									
+									// If you're done then break
+									if(fileSizeRemaining==0){
+										break;
+									}
+								} 
+								downloadingFile.close();
+								
+		 				}
+					}/*else if(!Client.subscribing&send){
+						break;
+					}*/
 					
 				}
-				
-			}
+			
 		
-
-		} catch (UnknownHostException e) {
+		}catch (UnknownHostException e){
 			e.printStackTrace();
-		} catch (IOException e) {
+		}catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
-
-	// ONLY USEFUL WHEN ITS FETCH COMMAND
+	
+	//ONLY USEFUL WHEN ITS FETCH COMMAND
 	private static int setChunkSize(long fileSizeRemaining) {
 		// determine the chunksize
-		int chunkSize = 1024 * 1024;
-		// if the file size remaining is less than chunksize reset it
-		if (fileSizeRemaining < chunkSize) {
-			chunkSize = (int) fileSizeRemaining;
+		int chunkSize = 1024*1024;
+		//if the file size remaining is less than chunksize reset it
+		if (fileSizeRemaining<chunkSize) {
+			chunkSize=(int) fileSizeRemaining;
 		}
 		return chunkSize;
 	}
+
 
 }
